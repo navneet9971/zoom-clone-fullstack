12a98d34c353cba14cf7024a8b8822f23c5c2121
navneet9971 <Navneetjha2012@gmail.com>
2024-05-29 03:36:51 +0530

    Stream Create Meeting is done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,15 @@
-import React, {ReactNode} from 'react'
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import React, { ReactNode } from 'react'
 
-const RootLayout = ({ children }: { children: 
-    ReactNode }) => {
+const RootLayout = ({ children }: {
+  children:
+  ReactNode
+}) => {
   return (
     <main>
+      <StreamVideoProvider>
         {children}
+      </StreamVideoProvider>
     </main>
   )
 }
